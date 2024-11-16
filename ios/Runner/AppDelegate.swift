d565454b6cc7f627294fe9ccb6f52672cba835e4
Bairak Nassour <bairak23455@gmail.com>
2024-11-16 10:29:13 +0200

    16/11/2024

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
+
+
 import Flutter
 import UIKit
-import Firebase
-import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,21 +9,7 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // تهيئة Firebase
-    FirebaseApp.configure()
-    
-    // إعداد delegate للإشعارات
-    UNUserNotificationCenter.current().delegate = self
-    
-    // تسجيل الجهاز لاستقبال الإشعارات
-    application.registerForRemoteNotifications()
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // دالة ربط توكين APNs مع Firebase Messaging
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
   }
 }
